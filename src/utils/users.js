834615e39d31c0f93bf7f1e35e49723ceda63f7c
vladimir86ns <vladimir86ns@gmail.com>
2019-04-23 18:38:29 +0200

    Storing Users: Part II

--- src/utils/users.js
+++ src/utils/users.js
@@ -27,16 +27,26 @@ const addUser = ({id, username, room}) => {
     return { user };
 };
 
-const removeUser = id => {
-    const index = users.findIndex(user => user.id === id);
+const removeUser = (id) => {
+    const index = users.findIndex((user) => user.id === id)
 
-    if (index != -1) {
+    if (index !== -1) {
         return users.splice(index, 1)[0]
     }
-};
+}
 
-addUser({
-    id: 1,
-    userName: 'Vladimir',
-    room: 'jedan'
-});
+const getUser = (id) => {
+    return users.find((user) => user.id === id)
+}
+
+const getUsersInRoom = (room) => {
+    room = room.trim().toLowerCase()
+    return users.filter((user) => user.room === room)
+}
+
+module.exports = {
+    addUser,
+    removeUser,
+    getUser,
+    getUsersInRoom
+}
